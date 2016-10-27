df36a9029db68f71c74eb10a60393a0ec373966f
Jan Stocker <janstocker@gmx.ch>
2016-10-27 16:32:43 +0200

    some work

--- site.js
+++ site.js
@@ -40,4 +40,42 @@ $(document).ready(function() {
         }
     });
 
+    $("#btLoad").click(function() {
+        console.log("btn");
+    });
+
+    $("#ddDevices").click(function() {
+        console.log("Selected Option:"+val($(this).HTML()));
+
+/*        var url = 'http://noahhome.selfhost.bz:8080/ekg3000/getfilelist.php';
+
+        $.ajax({
+            url: 'proxy.php',
+            type: 'GET',
+            data: {
+                address: url
+            },
+            success: function(response) {
+                // response now contains full HTML of google.com
+                console.log(response);
+                var res = JSON.parse(response);
+            
+                var list = document.getElementById("ddFiles"); 
+
+                for (i = 0; i < res.length; i++) {
+                    var opt = res[i].name;  
+                    var li = document.createElement("li");
+                    var link = document.createElement("a");   
+                    var oc = "ddDevicesSelected(\"" + opt + "\")";
+                    link.setAttribute("onclick", oc);          
+                    var text = document.createTextNode(opt);
+                    link.appendChild(text);
+                    link.href = "#";
+                    li.appendChild(link);
+                    list.appendChild(li);
+                }
+            }
+        });*/
+    });
+
 });
