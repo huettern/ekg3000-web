9e518d4c967c800cfef65f1e01e53c19b73a92a8
Jan Stocker <janstocker@gmx.ch>
2016-11-03 15:36:03 +0100

    c3 work, but with a lot of datas too slow

--- js/site.js
+++ js/site.js
@@ -1,14 +1,33 @@
+// var jsonFile = 0;
 
 
 function ddDevicesSelected(text) {
-    var btn = document.getElementById("bDevices"); 
+    var btn = document.getElementById("bDevices");
     // btn.value(text);
     $('#bDevicesText').text(text);
 }
 
+function plot(jsonF) {
+    console.log(jsonF);
+    console.log(jsonF.ekg.data);
 
-    
-$(document).ready(function() { 
+      var chart = c3.generate({
+               size: {
+                 height: 400
+               },
+               bindto: '#chart',
+               data: {
+                columns: [jsonF.ekg.data]
+               },
+
+               zoom: {
+                 enabled: true,
+               }
+             });
+
+}
+
+$(document).ready(function() {
     /* is executed after page is loaded */
     var url = 'http://noahhome.selfhost.bz:8080/ekg3000/getdevices.php';
 
@@ -21,29 +40,30 @@ $(document).ready(function() {
         success: function(response) {
             // response now contains full HTML of google.com
             var res = JSON.parse(response);
-        
-            var list = document.getElementById("#ddDevices"); 
+
+            var list = document.getElementById("#ddDevices");
 
             for (i = 0; i < res.length; i++) {
-                var opt = res[i].name;  
-              //  var sel = document.createElement("select");
+                var opt = res[i].name;
+                //  var sel = document.createElement("select");
                 //li.setAttribute("id", "ddLi" + res[i].id);   
                 $("#ddDevices").append($('<option>', {
-                    value: res[i].id,
-                    text: res[i].name
-                }))
-               // sel.setAttribute("class", "ddLi"); 
-               // sel.setAttribute("value", res[i].id)  
-                //var link = document.createElement("a");          
-               // var text = document.createTextNode(opt);
-                //link.appendChild(text);
-                //link.href = "#";
-                //li.appendChild(link);
-               // sel.appendChild(opt);
-                //list.appendChild(sel);
+                        value: res[i].id,
+                        text: res[i].name
+                    }))
+                    // sel.setAttribute("class", "ddLi"); 
+                    // sel.setAttribute("value", res[i].id)  
+                    //var link = document.createElement("a");          
+                    // var text = document.createTextNode(opt);
+                    //link.appendChild(text);
+                    //link.href = "#";
+                    //li.appendChild(link);
+                    // sel.appendChild(opt);
+                    //list.appendChild(sel);
             }
         }
     });
+
 
     $("#ddDevices").change(function() {
         var sDevName = $("#ddDevices option:selected").text();
@@ -60,16 +80,16 @@ $(document).ready(function() {
             success: function(response) {
                 // response now contains full HTML of google.com
                 var res = JSON.parse(response);
-            
-                var list = document.getElementById("ddFiles"); 
+
+                var list = document.getElementById("ddFiles");
 
                 $('#ddFiles').empty().append('<option disabled selected value>Select..</option>');
 
-               for (i = 0; i < res.length; i++) {
+                for (i = 0; i < res.length; i++) {
                     var opt = res[i].name;
                     $("#ddFiles").append($('<option>', {
                         value: res[i].id,
-                        text: res[i].time + '  |  ' + (res[i].file).split("/").pop() 
+                        text: res[i].time + '  |  ' + (res[i].file).split("/").pop()
                     }));
                 }
             }
@@ -78,7 +98,6 @@ $(document).ready(function() {
 
 
     $("#btLoad").click(function() {
-        console.log("btn");
         var sFileID = $("#ddFiles option:selected").val();
         var url = 'http://noahhome.selfhost.bz:8080/ekg3000/getfile.php' + '?file_id=' + sFileID;
 
@@ -88,14 +107,13 @@ $(document).ready(function() {
             data: {
                 address: url
             },
-   //         dataType: 'json; charset=utf-8',
+            //         dataType: 'json; charset=utf-8',
             success: function(response) {
                 // response now contains full HTML of google.com
-                var res = JSON.parse(response);
-
+                jsonFile = JSON.parse(response);
+                plot(jsonFile);
             }
-        });
-        
-    });
+        });   
 
+    });
 });
